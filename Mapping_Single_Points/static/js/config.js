aff8928aa5daf4656df9e0113b37ab4cb44f60b2
Miguel Montoya <miguel.montoya@artexturewalls.com>
2022-01-20 00:36:04 -0500

    mapping_multiple_points

--- Mapping_Single_Points/static/js/config.js
+++ Mapping_Single_Points/static/js/config.js
@@ -1,2 +1,2 @@
 //Api Key
-const API_KEY = "pk.eyJ1IjoicXVpeGNrMjMiLCJhIjoiY2t5ajRmZ2M4MjAwcDJ2cXBsYmxmcDVkcyJ9.nxjpllxzsEE6sGVn4NBCoQ";
+const API_KEY = "pk.eyJ1IjoicXVpeGNrMjMiLCJhIjoiY2t5bWlnYnlwMDJ0cTJubGozb3h4dXU2YiJ9.XTvQheQA_hGQbLPevPmEtQ";
